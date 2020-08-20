ddbd58b05a02a90905512d0b33171da6f1492af0
jrbeverly <jrbeverly@live.com>
2020-08-20 18:54:47 -0400

    Instance and firewall configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,19 +3,62 @@ provider "aws" {
 }
 
 locals {
-  name = "codespace"
-  domain  = "jrbeverly.dev"
-  region  = "ca-central-1"
-  availability_zone  = "ca-central-1b"
+  name              = "codespace"
+  domain            = "jrbeverly.dev"
+  url               = "${local.name}.${local.domain}"
+  region            = "ca-central-1"
+  availability_zone = "ca-central-1a"
 }
 
 resource "aws_lightsail_instance" "app" {
-  name              = "${local.name}.${local.domain}"
+  name              = local.url
   blueprint_id      = "ubuntu_18_04"
   bundle_id         = "micro_2_0"
-  key_pair_name     = local.keyname
   availability_zone = local.availability_zone
+  tags = {
+    name = local.name
+    www  = local.url
+    desc = "Cloud environment for running VSCode in the browser."
+  }
 }
+
+## WORKAROUND
+# Port configuration for the lightsail instance
+locals {
+  ports = [
+    {
+      fromPort = 22
+      toPort   = 22
+      protocol = "tcp"
+    },
+    {
+      fromPort = 80
+      toPort   = 80
+      protocol = "tcp"
+    },
+    {
+      fromPort = 443
+      toPort   = 443
+      protocol = "tcp"
+    },
+  ]
+  public_ports = "${join(" ", [for s in local.ports : format("fromPort=%d,toPort=%d,protocol=%s", s.fromPort, s.toPort, s.protocol)])}"
+}
+
+resource "null_resource" "firewall" {
+  triggers = {
+    public_ports = local.public_ports
+    url          = local.url
+  }
+
+  // Workaround until terraform-provider-aws/issues/700 (aws_lightsail provider should support open port management)
+  // has been resolved. For now use awscli to manually set these properties.
+  provisioner "local-exec" {
+    command = "aws lightsail put-instance-public-ports --instance-name=${local.url} --port-infos ${local.public_ports}"
+  }
+}
+
+###
 
 
 resource "aws_lightsail_static_ip_attachment" "app" {
